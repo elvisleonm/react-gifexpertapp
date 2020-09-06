b30d82de293f1fb36efcb9bfc447ba4a8d15b213
Elvis <elvisleonm@gmail.com>
2020-09-05 20:56:08 -0500

    docs agregados

---
+++ docs/precache-manifest.650ab01a4532a514d081adb6c6270768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6afcdb50411be33d4132367d487b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443a94ae2c3722366399",
+    "url": "/static/css/main.a4ca3ba7.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443a94ae2c3722366399",
+    "url": "/static/js/main.c33c00f5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
